8aee538891b16f1f27ed8af23a5ba8a1fec5cb93
KirtiGupta1111 <165828947+KirtiGupta1111@users.noreply.github.com>
2024-06-20 16:59:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,111 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require('express');
+const cors = require("cors");
+require('./db/config');
+const User = require('./db/User')
+const Products = require('./db/Products')
+const app = express();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+app.use(express.json());
+app.use(cors());
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+app.post("/register", async(req,res)=>{
+  const user = new User(req.body);
+    let result = await user.save();
+    result=result.toObject();
+    delete result.password
+  res.send(result);
+})
+
+app.post("/login", async (req, res)=>{
+  if(req.body.password && req.body.email){
+let user = await User.findOne(req.body).select("-password");  
+if(user) {
+    res.send(user)
+  } else {
+  res.send({result : "No User Found"})
+}
+} else {
+  res.send({result : "No User Found"})
+
+}
+}
+)
+
+
+app.post("/add-product", async (req,res)=>{
+  let product = new Products(req.body);
+  let result = await product.save();
+  res.send(result);
+})
+
+
+app.get("/products",async(req, res)=>{
+  const products = await Products.find();
+  if(products.length > 0)
+    {
+      res.send(products)
+    }
+    else{
+      res.send({result:"No Result Found"})
+    }
+})
+
+
+app.delete("/product/:id",async(req, res)=>{
+  let result =await Products.deleteOne({_id:req.params.id})
+  res.send(result)
+})
+
+
+app.get("/product/:id", async (req, res)=>{
+let result = await Products.findOne({_id:req.params.id})
+if(result)
+{
+  res.send(result)
+}
+else
+{
+  res.send({result : "No Record Found."})
+}
+
+})
+
+
+
+app.put("/product/:id", async(req,res)=>{
+  let result =await Products.updateOne(
+    {
+      _id:req.params.id
+    },
+    {
+      $set:req.body
+    }
+  )
+    res.send(result);
+})
+
+
+
+
+app.get("/search/:key", async(req,res)=>{
+  let result = await Products.find({
+    "$or":[
+      {
+      name : {$regex: req.params.key}
+      },
+
+      {
+        company: {$regex: req.params.key}
+
+      },
+
+      {
+        category: {$regex: req.params.key}
+
+      }
+
+    ]
+  })
+  res.send(result);
+})
+app.listen(3000);
